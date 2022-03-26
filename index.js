eb3a038cc64526402043ca02a0efbb892939e4f6
Edward Angelo Salas <zyro670@gmail.com>
2022-03-25 23:06:32 -0400

    commit tree c9e7b26ee10506396b62d9ef9f490c5232936485

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+const employee = {};
+
+function beforeEach(employee) {
+    for (const key in employee) {
+      delete employee[key];
+    }
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    var newemp = employee;
+    newemp = { 
+        "name": "Sam",
+        "streetAddress": "11 Broadway",
+    }
+    return newemp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    var employee = {};
+    employee = { 
+        "name": "Sam",
+        "streetAddress": "11 Broadway",
+    }
+    var anotheremp = employee;
+    delete anotheremp[key];
+    return anotheremp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    employee.key = undefined;
+    return employee;
+}
